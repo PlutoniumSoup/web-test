ff223f2af61a1d22eadc7d1d4b013858e9c1e96f
PlutoniumSoap <alephnight.g@gmail.com>
2025-05-30 03:46:41 +0300

    small changes

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -5,7 +5,7 @@ import Navbar from '../components/Common/Navbar'; // Создадим позже
 
 const MainLayout: React.FC = () => {
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen bg-[#7d7d7d]">
             <Navbar />
             <main className="container mx-auto p-4">
                 <Outlet /> {/* Здесь будут рендериться дочерние роуты */}
